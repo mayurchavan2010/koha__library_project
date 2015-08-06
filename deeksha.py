58e0fd0b3f6c1f5401c5c7be798e9791d01b1278
shailesh cheke <shailesh.cheke@gmail.com>
2015-08-06 13:02:45 +0530

    Modified recently

--- deeksha.py
+++ deeksha.py
@@ -3,9 +3,10 @@ db=MySQLdb.connect("192.168.8.45","deeksha","123","deeksha")
 dbLo=MySQLdb.connect("localhost","root","","shailesh")
 curs=db.cursor()
 curaLocl=dbLo.cursor()
-curs.execute("select Accession,BookNo,BookName,Author_FirstName1,Author_LastName1,Author_MiddleName1,Author_FirstName2,Author_LastName2,Author_MiddleName2,CallNo from lib_booktitle")
-for i in curs.fetchall():
-	authorsList=""
+#curs.execute("select Accession,BookNo,BookName,Author_FirstName1,Author_LastName1,Author_MiddleName1,Author_FirstName2,Author_LastName2,Author_MiddleName2,CallNo from lib_booktitle")
+#for i in curs.fetchall():
+#	print i
+#	authorsList=""
 	#if i[3] != "None" or i[3] != "":
 	#	print i[3]
 	#	authorsList=authorsList+i[3]
@@ -17,7 +18,8 @@ for i in curs.fetchall():
 	#        authorsList=authorsList + i[5]	
 	#print authorsList	
 #	authorsList=i[3]+" "+i[4]+" "+" "+i[5]
-	curaLocl.execute("insert into book_info values("+str(i[0])+","+str(i[1])+",'"+i[2]+"','shailesh cheke',"+str(i[9])+")")
-
+	
+	#curaLocl.execute("insert into book_info values("+str(i[0])+","+str(i[1])+",'"+i[2]+"','shailesh cheke',"+str(i[9])+")")
+curaLocl.execute("insert into shailesh.book_info(accession,BookNo) values(123,123)")
 dbLo.close()
 db.close()
